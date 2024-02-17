afdcd1ce3e880948e64c42ab367f1b990a319529
Marta Parra <149329102+Macata47@users.noreply.github.com>
2024-02-17 14:26:38 +0100

    form create

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Themes;
+
+// class Resource extends Model
+// {
+//     use HasFactory;
+// }
 
 class Resource extends Model
 {
-    use HasFactory;
+    public function theme()
+    {
+        return $this->belongsTo(Themes::class, 'id_themes');
+    }
+
+    public function extraResource()
+    {
+        return $this->belongsTo(ExtraResource::class, 'id_extraresources');
+    }
 }
