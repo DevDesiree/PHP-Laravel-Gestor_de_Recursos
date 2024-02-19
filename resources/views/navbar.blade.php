74c79aec2d50e5a906115260a1f34d1b891b76b2
yamiranea <yamilarenea@gmail.com>
2024-02-19 21:22:51 +0100

    main button styles

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="/resources/css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet">
     <title>navbar</title>
 
 </head>
@@ -13,17 +16,15 @@
 <body>
 
 
-    <nav class="border-gray-200 bg-black dark:bg-gray-800 dark:border-gray-700">
-        <div class="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
-            <div>
-                <a href="{{ route('index') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <nav class="bg-black w-auto">
+        <div class="flex flex-wrap justify-between items-center w-screen py-4">
+            <div class="flex flex-row items-center">
+                <a href="{{ route('index') }}" class="flex items-center">
                     <img src="{{ asset('img/logo-codecrafthers.png') }}" class="h-24 bg-white shadow-2xl" />
                 </a>
+                <h1 class="text-white navbar-title">Administrador de recursos</h1>
             </div>
-            <div class="mr-96">
-                <h1 class="text-white">Administrador de recursos</h1>
-            </div>
-            <div class="w-full md:w-auto ml-36">
+            <div class="pr-16">
                 <a href="{{ route('resources.create') }}">
                     <button
                         class="bg-yellow-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl items-end">
